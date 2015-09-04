ed7f4324b2d68f3997dcedd3139ed341f49eba1d
gregtucker <gregteucro@yahoo.com>
2015-09-04 16:54:01 -0600

    further work on reader

--- child_reader/child_reader.py
+++ child_reader/child_reader.py
@@ -10,7 +10,7 @@ Created on Thu Aug 20 16:23:47 2015
 """
 
 import os
-from numpy import zeros
+from numpy import zeros, ones, array
 
 _BASIC_CHILD_EXTENSIONS = [ 'nodes',
                       'edges',
@@ -38,9 +38,11 @@ class ChildRun(object):
         # triangles.
         self.number_of_nodes = self.read_number_of_nodes()
         self.number_of_edges = self.read_number_of_edges()
+        self.number_of_triangles = self.read_number_of_triangles()
         
         # create the arrays needed to store the data for one time slice
         self.create_data_arrays()
+        
         
     def open_child_files(self, name):
         """
@@ -64,6 +66,12 @@ class ChildRun(object):
             self.edgefile = open(basename+'.edges', 'r')
             self.trifile = open(basename+'.tri', 'r')
             self.zfile = open(basename+'.z', 'r')
+            self.areafile = open(basename+'.area', 'r')
+            self.netfile = open(basename+'.net', 'r')
+            self.qfile = open(basename+'.q', 'r')
+            self.slopefile = open(basename+'.slp', 'r')
+            self.taufile = open(basename+'.tau', 'r')
+            self.vareafile = open(basename+'.varea', 'r')
         except IOError:
             print('Can not find one or more files for run called '+basename)
             raise IOError
@@ -74,6 +82,15 @@ class ChildRun(object):
         Reads data for the next timeslice for the current run.
         """
         self.read_node_data()
+        self.read_drainage_areas()
+        self.read_flow_dirs()
+        self.read_discharges()
+        self.read_slopes()
+        self.read_shear_stresses()
+        self.read_voronoi_areas()
+        self.read_elevations()
+        self.read_edge_data()
+        self.read_triangle_data()
         
         
     def read_node_data(self):
@@ -86,7 +103,6 @@ class ChildRun(object):
         
         # read the number of nodes
         nn = int(self.nodefile.readline())
-        print 'nn', nn
         if nn!=self.number_of_nodes:
             # Number of nodes has changed; need to re-size the node arrays
             self.number_of_nodes = nn
@@ -96,14 +112,95 @@ class ChildRun(object):
         for n in xrange(nn):
             line = self.nodefile.readline()
             line = line.split()
-            print line
             assert len(line)==4, 'Error in node file'
             self.x[n] = float(line[0])
             self.y[n] = float(line[1])
             self.edg_at_node[n] = int(line[2])
             self.bnd[n] = int(line[3])
             
-    
+            
+    def read_drainage_areas(self):
+        """
+        Reads data for drainage area from .area file.
+        """
+        tm = self.areafile.readline()
+        assert float(tm)==self.current_time, 'Time in .area file does not match current time'
+        self.number_of_core_nodes = int(self.areafile.readline())
+        for n in range(self.number_of_core_nodes):
+            self.drainage_area[n] = float(self.areafile.readline())
+            
+            
+    def read_flow_dirs(self):
+        """
+        Reads direction of flow (receiver node ID) from .net file.
+        """
+        tm = self.netfile.readline()
+        assert float(tm)==self.current_time, 'Time in .area file does not match current time'
+        self.number_of_core_nodes = int(self.netfile.readline())
+        for n in range(self.number_of_core_nodes):
+            self.drains_to[n] = int(self.netfile.readline())
+        
+            
+    def read_discharges(self):
+        """
+        Reads data for discharge from .q file.
+        """
+        tm = self.qfile.readline()
+        assert float(tm)==self.current_time, 'Time in .area file does not match current time'
+        nn = int(self.qfile.readline())
+        assert nn==self.number_of_nodes, 'Mismatch in node numbers in q file'
+        for n in range(nn):
+            self.q[n] = float(self.qfile.readline())
+            
+            
+    def read_slopes(self):
+        """
+        Reads data for slope from .slp file.
+        """
+        tm = self.slopefile.readline()
+        assert float(tm)==self.current_time, 'Time in .area file does not match current time'
+        nn = int(self.slopefile.readline())
+        assert nn==self.number_of_nodes, 'Mismatch in node numbers in .slp file'
+        for n in range(nn):
+            self.slope[n] = float(self.slopefile.readline())
+            
+            
+    def read_shear_stresses(self):
+        """
+        Reads data for shear stress from .tau file.
+        """
+        tm = self.taufile.readline()
+        assert float(tm)==self.current_time, 'Time in .area file does not match current time'
+        nn = int(self.taufile.readline())
+        assert nn==self.number_of_nodes, 'Mismatch in node numbers in .slp file'
+        for n in range(nn):
+            self.tau[n] = float(self.taufile.readline())
+            
+            
+    def read_voronoi_areas(self):
+        """
+        Reads data for voronoi cell area from .varea file.
+        """
+        tm = self.vareafile.readline()
+        assert float(tm)==self.current_time, 'Time in .area file does not match current time'
+        nn = int(self.vareafile.readline())
+        assert nn==self.number_of_nodes, 'Mismatch in node numbers in .slp file'
+        for n in range(nn):
+            self.voronoi_area[n] = float(self.vareafile.readline())
+            
+            
+    def read_elevations(self):
+        """
+        Reads data for elevations from .z file.
+        """
+        tm = self.zfile.readline()
+        assert float(tm)==self.current_time, 'Time in .area file does not match current time'
+        nn = int(self.zfile.readline())
+        assert nn==self.number_of_nodes, 'Mismatch in node numbers in .slp file'
+        for n in range(nn):
+            self.z[n] = float(self.zfile.readline())
+            
+            
     def read_current_time(self):
         """
         Reads and returns the current time in the output files.
@@ -163,6 +260,26 @@ class ChildRun(object):
         return ne
         
         
+    def read_number_of_triangles(self):
+        """
+        Reads and returns number of triangles in current time slice.
+        """
+        # Remember position in file
+        curpos = self.trifile.tell()
+        
+        # Read the line that should be time
+        self.trifile.readline()
+        
+        # Read the line that should be number of nodes
+        nt = int(self.trifile.readline())
+        print 'there are',nt, 'edges'
+        
+        # Return to the previous position
+        self.trifile.seek(curpos)
+        
+        return nt
+        
+        
     def create_node_arrays(self):
         """Creates (or re-creates) the arrays of length equal to the 
         number of nodes."""
@@ -174,15 +291,111 @@ class ChildRun(object):
         self.z = zeros(nn)
         self.edg_at_node = zeros(nn, dtype=int)
         self.bnd = zeros(nn, dtype=int)
+        
+        # Create array for drainage areas
+        self.drainage_area = zeros(nn)
+
+        # Create array for drainage directions (ID of receiver node)
+        self.drains_to = -ones(nn, dtype=int)
+
+        # Create array for discharges
+        self.q = zeros(nn)
+
+        # Create array for slopes
+        self.slope = zeros(nn)
+
+        # Create array for shear stresses
+        self.tau = zeros(nn)
+
+        # Create array for Voronoi cell areas
+        self.voronoi_area = zeros(nn)
+
+        
+    def create_edge_arrays(self):
+        """Creates (or re-creates) the arrays of length equal to the 
+        number of edges."""
+        
+        # Create arrays for node-based info
+        ne = self.number_of_edges
+        self.edge_tail = zeros(ne, dtype=int)
+        self.edge_head = zeros(ne, dtype=int)
+        self.ccw_edge = zeros(ne, dtype=int)
+
+        
+    def create_triangle_arrays(self):
+        """Creates (or re-creates) the arrays of length equal to the 
+        number of triangles."""
+        
+        # Create arrays for node-based info
+        nt = self.number_of_triangles
+        self.tri_vertex = zeros((nt, 3), dtype=int)
+        self.tri_edge = zeros((nt, 3), dtype=int)
+        self.tri_tri = zeros((nt, 3), dtype=int)
 
         
     def create_data_arrays(self):
         """Creates the various arrays needed to store CHILD variables for one
         time slice."""
         self.create_node_arrays()
+        self.create_edge_arrays()   
+        self.create_triangle_arrays()
         
-        # Create arrays for...
+
+    def read_edge_data(self):
+        """
+        Reads edge data for the current time slice from the .edges file.
+        """
+        # read the current time
+        tm = float(self.edgefile.readline())
+        self.current_time = tm
         
+        # read the number of nodes
+        ne = int(self.edgefile.readline())
+        if ne!=self.number_of_edges:
+            # Number of nodes has changed; need to re-size the node arrays
+            self.number_of_edges = ne
+            self.create_edge_arrays()
+        
+        # read info for all the nodes
+        for e in xrange(ne):
+            line = self.edgefile.readline()
+            line = line.split()
+            assert len(line)==3, 'Error in edge file'
+            self.edge_tail[e] = int(line[0])
+            self.edge_head[e] = int(line[1])
+            self.ccw_edge[e] = int(line[2])
+            
+            
+    def read_triangle_data(self):
+        """
+        Reads edge data for the current time slice from the .edges file.
+        """
+        # read the current time
+        tm = float(self.trifile.readline())
+        self.current_time = tm
+        
+        # read the number of nodes
+        nt = int(self.trifile.readline())
+        if nt!=self.number_of_triangles:
+            # Number of nodes has changed; need to re-size the node arrays
+            self.number_of_triangles = nt
+            self.create_triangle_arrays()
+        
+        # read info for all the nodes
+        for t in xrange(nt):
+            line = self.trifile.readline()
+            line = line.split()
+            assert len(line)==9, 'Error in tri file'
+            self.tri_vertex[t][:] = array(line[0:3], dtype=int)
+            self.tri_edge[t][:] = array(line[3:6], dtype=int)
+            self.tri_tri[t][:] = array(line[6:9], dtype=int)
+            
+        print self.tri_vertex
+        print self.tri_edge
+        print self.tri_tri
+        
+            
+            
         
         
 def child_files_exist_with_name(the_name):
--- child_reader/test_childreader.py
+++ child_reader/test_childreader.py
@@ -21,14 +21,190 @@ def test_child_reader():
     # Read the first time step
     cr.read_next_timeslice()
 
+    # Test data from .nodes file
     assert cr.current_time==0.0, 'time error'
     assert cr.number_of_nodes==9, 'num nodes should be 9'
+    assert cr.number_of_edges==34, 'num edges should be 34'
+    assert cr.number_of_triangles==9, 'num tri should be 9'
+
+    # Test data from .nodes file
     assert_array_equal(np.round(cr.x, decimals=1), \
                        np.array([480.8, 0.0, 0.0, 0.0, 500.0, 1000.0, 1000.0, \
                        1000.0, 500.0]))
+    assert_array_equal(np.round(cr.y, decimals=1), \
+                       np.array([523.5, 0.0, 500.0, 1000.0, 1000.0, 1000.0, 0.0, \
+                       500.0, 0.0]))
+    assert_array_equal(cr.edg_at_node, np.array([0, 12, 1, 3, 5, 7, 28, 9, 11], \
+                       dtype=int))
+    assert_array_equal(cr.bnd, np.array([0, 1, 1, 1, 1, 1, 1, 1, 2], dtype=int))
     
+    # Test data from .area file
+    assert_array_equal(np.round(cr.drainage_area, decimals=1), \
+                       np.array([249753.7, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]))
+                       
+    # Test data from .net file
+    assert_array_equal(cr.drains_to, np.array([8, -1, -1, -1, -1, -1, -1, -1, -1]))
+                       
+     # Test data from .q (discharge) file
+    assert_array_equal(np.round(cr.q, decimals=1), np.zeros(9))
+                       
+     # Test data from .slp (slope) file
+    assert_array_equal(np.round(cr.slope, decimals=3), np.zeros(9))
+                       
+     # Test data from .tau (shear stress) file
+    assert_array_equal(np.round(cr.tau, decimals=1), np.zeros(9))
+                       
+    # Test data from .varea (Voronoi cell area) file
+    assert_array_equal(np.round(cr.voronoi_area, decimals=1), \
+                       np.array([249753.7, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]))
+                       
+     # Test data from .z (elevation) file
+    assert_array_equal(np.round(cr.z, decimals=1), np.zeros(9))
+                       
+    # Test data from .edges file
+    assert_array_equal(cr.edge_tail, np.array([0, 2, 0, 3, 0, 4, 0, 5, 0, 7, \
+                                               0, 8, 1, 2, 1, 8, 2, 3, 2, 8, \
+                                               3, 4, 3, 5, 4, 5, 5, 7, 6, 7, \
+                                               6, 8, 7, 8], dtype=int))
+    assert_array_equal(cr.edge_head, np.array([2, 0, 3, 0, 4, 0, 5, 0, 7, 0, \
+                                               8, 0, 2, 1, 8, 1, 3, 2, 8, 2, \
+                                               4, 3, 5, 3, 5, 4, 7, 5, 7, 6, \
+                                               8, 6, 8, 7], dtype=int))
+    assert_array_equal(cr.ccw_edge, np.array([10, 16,  0, 20,  2, 24,  4, 26,  6, 32, \
+                                               8, 19, 14, 18, 12, 31, 13,  3,  1, 15, \
+                                              22,  5, 17, 25, 21,  7, 23,  9, 30, 27, \
+                                              28, 33, 29, 11], dtype=int))
+                                              
+    # Test data from .tri file
+    assert_array_equal(cr.tri_vertex, np.array([[0, 2, 8],
+                                                [0, 3, 2],
+                                                [0, 4, 3],
+                                                [0, 5, 4],
+                                                [0, 7, 5],
+                                                [0, 8, 7],
+                                                [1, 8, 2],
+                                                [3, 4, 5],
+                                                [6, 7, 8]], dtype=int))
+                                             
+    # Test data from .tri file
+    assert_array_equal(cr.tri_edge, np.array([[ 6,  5,  1],
+                                              [-1,  0,  2],
+                                              [ 7,  1,  3],
+                                              [ 7,  2,  4],
+                                              [-1,  3,  5],
+                                              [ 8,  4,  0],
+                                              [ 0, -1, -1],
+                                              [ 3, -1,  2],
+                                              [ 5, -1, -1]], dtype=int))
+                                             
+    # Test data from .tri file
+    assert_array_equal(cr.tri_tri,  np.array([[10,  1, 19],
+                                              [ 0,  3, 16],
+                                              [ 2,  5, 20],
+                                              [ 4,  7, 24],
+                                              [ 6,  9, 26],
+                                              [ 8, 11, 32],
+                                              [12, 15, 18],
+                                              [22, 21, 25],
+                                              [30, 29, 33]], dtype=int))
+
+    # Read the first time step
+    cr.read_next_timeslice()
+
+    # Test data from .nodes file
+    assert cr.current_time==1.0, 'time error'
+    assert cr.number_of_nodes==9, 'num nodes should be 9'
+    assert cr.number_of_edges==34, 'num edges should be 34'
+    assert cr.number_of_triangles==9, 'num tri should be 9'
+                                             
+    # Now try again with values
+
+    # Test data from .nodes file
+    assert_array_equal(np.round(cr.x, decimals=1), \
+                       np.array([480.8, 0.0, 0.0, 0.0, 500.0, 1000.0, 1000.0, \
+                       1000.0, 500.0]))
+    assert_array_equal(np.round(cr.y, decimals=1), \
+                       np.array([523.5, 0.0, 500.0, 1000.0, 1000.0, 1000.0, 0.0, \
+                       500.0, 0.0]))
+    assert_array_equal(cr.edg_at_node, np.array([0, 12, 1, 3, 5, 7, 28, 9, 11], \
+                       dtype=int))
+    assert_array_equal(cr.bnd, np.array([0, 1, 1, 1, 1, 1, 1, 1, 2], dtype=int))
+    
+    # Test data from .area file
+    assert_array_equal(np.round(cr.drainage_area, decimals=1), \
+                       np.array([249753.7, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]))
+                       
+    # Test data from .net file
+    assert_array_equal(cr.drains_to, np.array([8, -1, -1, -1, -1, -1, -1, -1, -1]))
+                       
+     # Test data from .q (discharge) file
+    assert_array_equal(np.round(cr.q, decimals=1), np.zeros(9))
+                       
+     # Test data from .slp (slope) file
+    assert_array_equal(np.round(cr.slope, decimals=3),  \
+                       np.array([0.002, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]))
+                       
+     # Test data from .tau (shear stress) file
+    assert_array_equal(np.round(cr.tau, decimals=1), np.zeros(9))
+                       
+    # Test data from .varea (Voronoi cell area) file
+    assert_array_equal(np.round(cr.voronoi_area, decimals=1), \
+                       np.array([249753.7, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]))
+                       
+     # Test data from .z (elevation) file: now increased by 1 at interior node
+    assert_array_equal(np.round(cr.z, decimals=1), \
+                       np.array([1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]))
+                       
+    # Test data from .edges file
+    assert_array_equal(cr.edge_tail, np.array([0, 2, 0, 3, 0, 4, 0, 5, 0, 7, \
+                                               0, 8, 1, 2, 1, 8, 2, 3, 2, 8, \
+                                               3, 4, 3, 5, 4, 5, 5, 7, 6, 7, \
+                                               6, 8, 7, 8], dtype=int))
+    assert_array_equal(cr.edge_head, np.array([2, 0, 3, 0, 4, 0, 5, 0, 7, 0, \
+                                               8, 0, 2, 1, 8, 1, 3, 2, 8, 2, \
+                                               4, 3, 5, 3, 5, 4, 7, 5, 7, 6, \
+                                               8, 6, 8, 7], dtype=int))
+    assert_array_equal(cr.ccw_edge, np.array([10, 16,  0, 20,  2, 24,  4, 26,  6, 32, \
+                                               8, 19, 14, 18, 12, 31, 13,  3,  1, 15, \
+                                              22,  5, 17, 25, 21,  7, 23,  9, 30, 27, \
+                                              28, 33, 29, 11], dtype=int))
+                                              
+    # Test data from .tri file
+    assert_array_equal(cr.tri_vertex, np.array([[0, 2, 8],
+                                                [0, 3, 2],
+                                                [0, 4, 3],
+                                                [0, 5, 4],
+                                                [0, 7, 5],
+                                                [0, 8, 7],
+                                                [1, 8, 2],
+                                                [3, 4, 5],
+                                                [6, 7, 8]], dtype=int))
+                                             
+    # Test data from .tri file
+    assert_array_equal(cr.tri_edge, np.array([[ 6,  5,  1],
+                                              [-1,  0,  2],
+                                              [ 7,  1,  3],
+                                              [ 7,  2,  4],
+                                              [-1,  3,  5],
+                                              [ 8,  4,  0],
+                                              [ 0, -1, -1],
+                                              [ 3, -1,  2],
+                                              [ 5, -1, -1]], dtype=int))
+                                             
+    # Test data from .tri file
+    assert_array_equal(cr.tri_tri,  np.array([[10,  1, 19],
+                                              [ 0,  3, 16],
+                                              [ 2,  5, 20],
+                                              [ 4,  7, 24],
+                                              [ 6,  9, 26],
+                                              [ 8, 11, 32],
+                                              [12, 15, 18],
+                                              [22, 21, 25],
+                                              [30, 29, 33]], dtype=int))
+                                             
+                                             
+
 if __name__=='__main__':
     test_child_reader()
 
-    
     
